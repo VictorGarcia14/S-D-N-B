cd10b28d0a03e59e083faf8d1d25b943b430a88d
VictorGarcia14 <vicmag1403@gmail.com>
2024-06-04 00:37:58 -0400

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 # Configuración de la conexión a MongoDB Atlas
-app.config['MONGO_URI'] = 'mongodb+srv://<usuario>:<contraseña>@<cluster>.mongodb.net/<base_de_datos>?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://victor1403:victor2003@<cluster>.mongodb.net/?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
 
